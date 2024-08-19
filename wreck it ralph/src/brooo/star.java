1bf35058c1d20bba651a4422d352f27feb1dd3eb
Stormtroopper <75199964+Stormtroopper@users.noreply.github.com>
2024-08-19 09:34:08 +0530

    Add files via upload

---
+++ wreck it ralph/src/brooo/star.java
@@ -0,0 +1,18 @@
+package brooo;
+
+public class star 
+{
+public static void main(String[] args)
+{
+	int m,n;
+	for(m=0;m<12;m++)
+	{
+		for(n=1;n<=m;n++)
+		{
+			System.out.println("*");
+		}
+			System.out.println();
+	}
+  }
+}
+
